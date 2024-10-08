0eb4531512bc64a8c12da2f2a676fe5a9799c11d
linuztx <linuztx@gmail.com>
2024-10-09 06:27:16 +0800

    Add caching for system information

--- clibot/system.py
+++ clibot/system.py
@@ -2,41 +2,56 @@ import subprocess
 import platform
 import datetime
 import os
+from functools import lru_cache
 
 package_dir = os.path.dirname(os.path.abspath(__file__))
 
-def get_system_clibot():
+@lru_cache(maxsize=1)
+def get_system_info():
     """Get system information."""
     system_name = platform.system()
     
     if system_name == "Windows":
-        os_info = subprocess.check_output('systeminfo', shell=True).decode('utf-8').splitlines()
-        os_name = next((line.split(':', 1)[1].strip() for line in os_info if line.startswith('OS Name:')), "Unknown OS")
-
-        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        kernel_info = subprocess.check_output('ver', shell=True).decode('utf-8').strip()
-        cpu_info = subprocess.check_output('wmic cpu get caption', shell=True).decode('utf-8').strip().split('\n')[1].strip()
-        gpu_info = subprocess.check_output('wmic path win32_videocontroller get caption', shell=True).decode('utf-8').strip().split('\n')[1].strip()
-        mem_info = subprocess.check_output('wmic memorychip get capacity', shell=True).decode('utf-8').strip().split('\n')[1].strip()
-        disk_info = subprocess.check_output('wmic logicaldisk get size,freespace,caption', shell=True).decode('utf-8').strip().split('\n')[1:]
-        disk_info = ', '.join(disk_info)
-
+        return get_windows_info()
     else:
-        try:
-            os_info = subprocess.check_output('cat /etc/os-release', shell=True).decode('utf-8').strip()
-            os_name = next((line.split('=')[1].strip('"') for line in os_info.split('\n') if line.startswith('PRETTY_NAME=')), "Unknown OS")
-        except subprocess.CalledProcessError:
-            os_name = "Unknown OS"
+        return get_unix_info()
 
-        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        kernel_info = subprocess.check_output('uname -a', shell=True).decode('utf-8').strip()
-        cpu_info_raw = subprocess.check_output('lscpu | grep "Model name"', shell=True).decode('utf-8').strip()
-        cpu_info = cpu_info_raw.split(':', 1)[1].strip() if ':' in cpu_info_raw else cpu_info_raw
-        gpu_info = subprocess.check_output(r"lspci | grep -i 'vga\|3d\|2d'", shell=True).decode('utf-8').strip()
-        mem_info_raw = subprocess.check_output('free -h | grep "Mem"', shell=True).decode('utf-8').strip().split()
-        mem_info = f"Total: {mem_info_raw[1]}, Used: {mem_info_raw[2]}, Free: {mem_info_raw[3]}, Shared: {mem_info_raw[4]}, Buffers: {mem_info_raw[5]}, Available: {mem_info_raw[6]}"
-        disk_info_raw = subprocess.check_output('df -h --total | grep "total"', shell=True).decode('utf-8').strip().split()
-        disk_info = f"Total: {disk_info_raw[1]}, Used: {disk_info_raw[2]}, Available: {disk_info_raw[3]}, Use%: {disk_info_raw[4]}, Mounted on: {disk_info_raw[5]}"
+def get_windows_info():
+    os_info = subprocess.check_output('systeminfo', shell=True).decode('utf-8').splitlines()
+    os_name = next((line.split(':', 1)[1].strip() for line in os_info if line.startswith('OS Name:')), "Unknown OS")
+
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    kernel_info = subprocess.check_output('ver', shell=True).decode('utf-8').strip()
+    cpu_info = subprocess.check_output('wmic cpu get caption', shell=True).decode('utf-8').strip().split('\n')[1].strip()
+    gpu_info = subprocess.check_output('wmic path win32_videocontroller get caption', shell=True).decode('utf-8').strip().split('\n')[1].strip()
+    mem_info = subprocess.check_output('wmic memorychip get capacity', shell=True).decode('utf-8').strip().split('\n')[1].strip()
+    disk_info = subprocess.check_output('wmic logicaldisk get size,freespace,caption', shell=True).decode('utf-8').strip().split('\n')[1:]
+    disk_info = ', '.join(disk_info)
+
+    return os_name, now, kernel_info, cpu_info, gpu_info, mem_info, disk_info
+
+def get_unix_info():
+    try:
+        os_info = subprocess.check_output('cat /etc/os-release', shell=True).decode('utf-8').strip()
+        os_name = next((line.split('=')[1].strip('"') for line in os_info.split('\n') if line.startswith('PRETTY_NAME=')), "Unknown OS")
+    except subprocess.CalledProcessError:
+        os_name = "Unknown OS"
+
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    kernel_info = subprocess.check_output('uname -a', shell=True).decode('utf-8').strip()
+    cpu_info_raw = subprocess.check_output('lscpu | grep "Model name"', shell=True).decode('utf-8').strip()
+    cpu_info = cpu_info_raw.split(':', 1)[1].strip() if ':' in cpu_info_raw else cpu_info_raw
+    gpu_info = subprocess.check_output(r"lspci | grep -i 'vga\|3d\|2d'", shell=True).decode('utf-8').strip()
+    mem_info_raw = subprocess.check_output('free -h | grep "Mem"', shell=True).decode('utf-8').strip().split()
+    mem_info = f"Total: {mem_info_raw[1]}, Used: {mem_info_raw[2]}, Free: {mem_info_raw[3]}, Shared: {mem_info_raw[4]}, Buffers: {mem_info_raw[5]}, Available: {mem_info_raw[6]}"
+    disk_info_raw = subprocess.check_output('df -h --total | grep "total"', shell=True).decode('utf-8').strip().split()
+    disk_info = f"Total: {disk_info_raw[1]}, Used: {disk_info_raw[2]}, Available: {disk_info_raw[3]}, Use%: {disk_info_raw[4]}, Mounted on: {disk_info_raw[5]}"
+
+    return os_name, now, kernel_info, cpu_info, gpu_info, mem_info, disk_info
+
+def get_system_clibot():
+    """Get system information for Clibot."""
+    os_name, now, kernel_info, cpu_info, gpu_info, mem_info, disk_info = get_system_info()
 
     system_prompt = prompt.format(
         os_name=os_name,
